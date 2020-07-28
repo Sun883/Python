e90e1fa44c008e7f7dd0a24d1388545b9e0c6748
Git Server Admin <admin@git-server>
2020-07-28 17:43:29 +0000

    Add initial repository

---
+++ prog.py
@@ -0,0 +1,24 @@
+import os
+#Write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+with open('.hidden.txt','w') as outfile:
+     outfile.write(s)
+with open('.hidden1.txt', 'w') as outfile:
+     outfile.write(q)
+with open('.hidden2.txt', 'w') as outfile:
+     outfile.write(p)
+with open('.hidden3.txt', 'w') as outfile:
+     outfile.write(w)
+
+
